e82b002cb515e7c627e41c34b1b57812a0788218
unknown <roshananand2005@gmail.com>
2024-11-11 11:53:55 +0530

    Made mouse animation

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -4,39 +4,46 @@ import React, { useEffect, useState } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 const Mouse = () => {
-  const [mouseX, setmouseX] = useState(10);
-  const [mouseY, setmouseY] = useState(10);
-  useEffect(() => {
-    const moveMouse = (e) => {
-      setmouseX(e.x);
-      setmouseY(e.y);
-    }
 
-    window.addEventListener("mousemove", moveMouse);
+  // useGSAP(() => {
+  //   gsap.to('.light', {
+  //     boxShadow: `1px 1px 50px 20px var(--accent-one-color)`,
+  //     duration: 0.3,
+  //     repeat: -1,
+  //     yoyo: true,
+  //   })
+  // })
+// 1/2  0.1
+// 2  0.2
+// 3  0.3
+  useEffect(() => {
+    const moveDivs = (e) => {
+      document.querySelectorAll('.moveing-div').forEach((div, i) => {
+        console.log((i+1)/2*0.2+"->"+i);
+        
+        gsap.to(div, {
+          top: `${e.y}px`,
+          left: `${e.x}px`,
+          duration: (i+1)/2 * 0.2,
+        })
+      })
+    }
+    window.addEventListener("mousemove", moveDivs);
     return () => {
-      window.removeEventListener("mousemove", moveMouse);
+      window.removeEventListener("mousemove", moveDivs);
     }
   }, [])
 
-  useGSAP(() => {
-    gsap.to('.light', {
-      boxShadow: `1px 1px 50px 20px var(--accent-one-color)`,
-      duration: 0.3,
-      repeat: -1,
-      yoyo: true,
-    })
-  })
 
-  return (
-    <section className='light fixed bg-accentOne w-[5px] h-[5px] rounded-full z-[1] blur-xl' style={{
-      boxShadow: `1px 1px 10px 10px var(--accent-one-color)`,
-      top: `${mouseY}px`,
-      left: `${mouseX}px`,
-      transform: `translate(-50%,-50%)`
+  let divs = [];
+  for (let i = 1; i <10; i++) {    
+    divs.push(<div key={i} className='moveing-div absolute rounded-full top-1/2 left-1/2 w-[30px] h-[30px] bg-accentTwo -translate-x-1/2 -translate-y-1/2 blur-[8px]' style={{
+      scale:1-`0.${i}`,
+      boxShadow:"0px 0px 15px 5px var(--accent-two-color)"
+    }}></div>)
+  }
 
-    }}>
-    </section >
-  )
+  return (<section className='z-[1]' >{divs}</section >)
 }
 
 export default Mouse
